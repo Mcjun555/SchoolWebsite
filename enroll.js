06a854062ce651c7a0f7521bcbecd8ba0d87aa58
Mcjun555 <81612025+Mcjun555@users.noreply.github.com>
2021-05-03 20:01:27 +0800

    Add files via upload

---
+++ enroll.js
@@ -0,0 +1,22 @@
+// JavaScript Document
+
+document.getElementById("name").value = "Last Name, First Name Middle Name";
+
+document.getElementById("age").value = "Age";
+
+document.getElementById("gender").value = "Gender";
+
+document.getElementById("phone").value = "+63";
+
+document.getElementById("email").value = "example@gmail.com";
+
+document.getElementById("mail").value = "Enter complete address";
+
+document.getElementById("prog").value = "Enter complete name of school";
+
+document.getElementById("id").value = "Enter 12-digit LRN";
+
+document.getElementById("year").value = "Grade - ";
+
+document.getElementById("sec").value = "Enter strand acronym";
+
